f11467907266b1d0df153dda793484965a360754
Lima <cleibsonslima@gmail.com>
2026-04-28 16:10:38 -0300

    Adding the characters to JavaScript makes the HTML element simpler and more concise.

---
+++ js/app.js
@@ -0,0 +1,83 @@
+// ====================================
+// VUE.JS EM RELAÇÃO AO JAVASCRIPT
+// ====================================
+
+/*
+Vue.js é um FRAMEWORK JavaScript.
+
+Isso significa que:
+- Ele é construído SOBRE o JavaScript
+- Usa JavaScript como base
+- Facilita a criação de interfaces dinâmicas (Front-end)
+
+Enquanto o JavaScript puro:
+- Manipula o DOM diretamente
+- Exige mais código para comportamento complexo
+
+O Vue:
+- Abstrai a manipulação do DOM
+- Trabalha com ligação de dados (data binding)
+- Organiza o código em uma estrutura mais clara
+- Torna o desenvolvimento mais produtivo e legível
+*/
+
+const LIST = [
+    {
+    id: 1,
+    nome: 'Mestre Yoda',
+    avatar: 'images/yoda.png'
+    },
+
+    {
+    id: 2,
+    nome: 'Luke Skywalker',
+    avatar: 'images/luke.png'
+    },
+
+    {
+    id: 3,
+    nome: 'Princesa Leia',
+    avatar: 'images/leia.png'
+    },
+
+    {
+    id: 4,
+    nome: 'Hansolo',
+    avatar: 'images/Hansolo.png'
+    },
+
+    {
+    id: 5,
+    nome: 'Chewbacca',
+    avatar: 'images/chewbacca.png'
+    },
+
+    {
+    id: 6,
+    nome: 'R2D2',
+    avatar: 'images/R2D2.png'
+    },
+
+    {
+    id: 7,
+    nome: 'R2D2',
+    avatar: 'images/R2D2.png'
+    },
+
+    {
+    id: 8,
+    nome: 'c3po',
+    avatar: 'images/c3po.png'
+    }
+]
+
+const App = new Vue({
+    el: '#app',
+    data: {
+        title: 'Star Wars Logo',
+        name:  'Cleibson Silva',
+        Idade: 'Cleibson Silva',
+        characters: LIST
+    }
+
+})
