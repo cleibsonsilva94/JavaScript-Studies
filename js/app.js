11e5555708045bad1497eb937bcb976d356dd061
Lima <cleibsonslima@gmail.com>
2026-04-29 15:00:14 -0300

    Adding a filter to the search

--- js/app.js
+++ js/app.js
@@ -23,51 +23,51 @@ O Vue:
 
 const LIST = [
     {
-    id: 1,
-    nome: 'Mestre Yoda',
-    avatar: 'images/yoda.png'
+        id: 1,
+        nome: 'Mestre Yoda',
+        avatar: 'images/yoda.png'
     },
 
     {
-    id: 2,
-    nome: 'Luke Skywalker',
-    avatar: 'images/luke.png'
+        id: 2,
+        nome: 'Luke Skywalker',
+        avatar: 'images/luke.png'
     },
 
     {
-    id: 3,
-    nome: 'Princesa Leia',
-    avatar: 'images/leia.png'
+        id: 3,
+        nome: 'Princesa Leia',
+        avatar: 'images/leia.png'
     },
 
     {
-    id: 4,
-    nome: 'Hansolo',
-    avatar: 'images/Hansolo.png'
+        id: 4,
+        nome: 'Hansolo',
+        avatar: 'images/Hansolo.png'
     },
 
     {
-    id: 5,
-    nome: 'Chewbacca',
-    avatar: 'images/chewbacca.png'
+        id: 5,
+        nome: 'Chewbacca',
+        avatar: 'images/chewbacca.png'
     },
 
     {
-    id: 6,
-    nome: 'R2D2',
-    avatar: 'images/R2D2.png'
+        id: 6,
+        nome: 'R2D2',
+        avatar: 'images/R2D2.png'
     },
 
     {
-    id: 7,
-    nome: 'Vader',
-    avatar: 'images/vader.png'
+        id: 7,
+        nome: 'Vader',
+        avatar: 'images/vader.png'
     },
 
     {
-    id: 8,
-    nome: 'c3po',
-    avatar: 'images/c3po.png'
+        id: 8,
+        nome: 'c3po',
+        avatar: 'images/c3po.png'
     }
 ]
 
@@ -76,11 +76,21 @@ const App = new Vue({
     data: {
         title: 'Star Wars Logo',
         name: 'Cleibson Silva',
-        characters: LIST
+        characters: LIST,
+        searchName: ''
     },
     methods: {
         like(userName) {
-        alert(`O personagem ${userName} recebeu um like!`)
+            alert(`O personagem ${userName} recebeu um like!`)
+        },
+        search() {
+            const list = this.characters
+
+            const result = list.filter(item => {
+                return item.nome === this.searcName
+            })
+            
+            this.characters = result
         }
     }
 })
